6ddb4010df461fb6a70df971e39e0eb8dbbc60c7
Lakshay Angrish <angrishlakshay@gmail.com>
2020-04-01 15:04:45 +0530

    display the ascii matrix

--- Ascii Art/art.cpp
+++ Ascii Art/art.cpp
@@ -4,7 +4,7 @@
 #include<string>
 using namespace Magick;
 
-const std::string ascii = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+const std::string ascii_map = ".`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
 
 int how_bright(double r, double g, double b) {
     return (0.21 * r + 0.72 * g + 0.07 * b) / 256;
@@ -26,22 +26,23 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    image.resize("500x170");
     std::cout << "Image Size = " << image.columns() << " X " << image.rows() << '\n';
 
     Magick::PixelPacket *pixels = image.getPixels(0, 0, image.columns(), image.rows());
     const int columns = image.columns();
     const int rows = image.rows();
 
-    int brightness[columns][rows];
+    int brightness;
     int char_idx = 0;
-    Magick::PixelPacket* pixel_array[columns][rows];
-    for (int i = 0; i != columns; i++) {
-        for (int j = 0; j != rows; j++) {
-            brightness[i][j] = how_bright(pixels->red, pixels->green, pixels->blue);
-            char_idx = (brightness[i][j] * 65) / 256;
-            // std::cout << ascii[char_idx] << '\n';
-            pixel_array[i][j] = pixels++;
+    for (int i = 0; i != rows; i++) {
+        for (int j = 0; j != columns; j++) {
+            brightness = how_bright(pixels->red, pixels->green, pixels->blue);
+            pixels++;
+            char_idx = (brightness * ascii_map.size()) / 256;
+            std::cout << ascii_map[char_idx] << ascii_map[char_idx] ;
         }
+        std::cout << '\n';
     }
 
     return 0;
