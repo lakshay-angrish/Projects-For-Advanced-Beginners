f742ae24f4f9383172068de72aa3d23a18a3ecf9
Lakshay Angrish <angrishlakshay@gmail.com>
2020-04-01 12:44:05 +0530

    calculate brightness from RGB

--- Ascii Art/art.cpp
+++ Ascii Art/art.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 #include<Magick++.h>
+#include<cmath>
+#include<string>
 using namespace Magick;
+
+const std::string ascii = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+
+int how_bright(double r, double g, double b) {
+    return (0.21 * r + 0.72 * g + 0.07 * b) / 256;
+}
 
 int main(int argc, char** argv) {
     InitializeMagick(*argv);
@@ -24,9 +32,12 @@ int main(int argc, char** argv) {
     const int columns = image.columns();
     const int rows = image.rows();
 
+    int brightness[columns][rows];
     Magick::PixelPacket* pixel_array[columns][rows];
     for (int i = 0; i != columns; i++) {
         for (int j = 0; j != rows; j++) {
+            brightness[i][j] = how_bright(pixels->red, pixels->green, pixels->blue);
+            // std::cout << brightness[i][j] << '\n';
             pixel_array[i][j] = pixels++;
         }
     }
