2b223092e97bafc88c099b70abce7e4c29191378
Lakshay Angrish <angrishlakshay@gmail.com>
2020-04-01 17:15:42 +0530

    Add colored display using ANSI codes

--- Ascii Art/art.cpp
+++ Ascii Art/art.cpp
@@ -4,10 +4,30 @@
 #include<string>
 using namespace Magick;
 
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+
+const std::string colors[] = {BLACK, BLUE, GREEN, CYAN, RED, MAGENTA, YELLOW, WHITE};
+
 const std::string ascii_map = ".`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
 
 int how_bright(double r, double g, double b) {
     return (0.21 * r + 0.72 * g + 0.07 * b) / 256;
+}
+
+int calc_color(int r, int g, int b) {
+    r /= 256 * 200;
+    g /= 256 * 200;
+    b /= 256 * 200;
+
+    return r * 4 + g * 2 + b * 1;
 }
 
 int main(int argc, char** argv) {
@@ -35,12 +55,14 @@ int main(int argc, char** argv) {
 
     int brightness;
     int char_idx = 0;
+    int color_idx = 8;
     for (int i = 0; i != rows; i++) {
         for (int j = 0; j != columns; j++) {
             brightness = how_bright(pixels->red, pixels->green, pixels->blue);
-            pixels++;
+            color_idx = calc_color(pixels->red, pixels->green, pixels->blue);
             char_idx = (brightness * ascii_map.size()) / 256;
-            std::cout << ascii_map[char_idx] << ascii_map[char_idx] ;
+            std::cout << colors[color_idx] << ascii_map[char_idx] << ascii_map[char_idx] << RESET ;
+            pixels++;
         }
         std::cout << '\n';
     }
