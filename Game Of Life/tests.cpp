00851ddf84d4f7b39f2bdeda33f7a41349a26060
Lakshay Angrish <angrishlakshay@gmail.com>
2020-04-02 16:49:03 +0530

    add some unit tests

---
+++ Game Of Life/tests.cpp
@@ -0,0 +1,54 @@
+#include "game.h"
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+void test1() {
+    vector<vector<int> > init_state;
+    init_state.push_back(vector<int>{0, 0, 0});
+    init_state.push_back(vector<int>{0, 0, 0});
+    init_state.push_back(vector<int>{0, 0, 0});
+
+    vector<vector<int> > expected_next_state;
+    expected_next_state.push_back(vector<int>{0, 0, 0});
+    expected_next_state.push_back(vector<int>{0, 0, 0});
+    expected_next_state.push_back(vector<int>{0, 0, 0});
+
+    next_state(init_state);
+
+    if (init_state == expected_next_state) {
+        cout << "PASSED 1\n";
+    } else {
+        cout << "FAILED 1\n";
+    }
+}
+
+void test2() {
+    vector<vector<int> > init_state;
+    init_state.push_back(vector<int>{0, 0, 1});
+    init_state.push_back(vector<int>{0, 1, 1});
+    init_state.push_back(vector<int>{0, 0, 0});
+
+    vector<vector<int> > expected_next_state;
+    expected_next_state.push_back(vector<int>{0, 1, 1});
+    expected_next_state.push_back(vector<int>{0, 1, 1});
+    expected_next_state.push_back(vector<int>{0, 0, 0});
+
+    next_state(init_state);
+
+    if (init_state == expected_next_state) {
+        cout << "PASSED 2\n";
+    } else {
+        cout << "FAILED 2\n";
+    }
+
+    render(init_state);
+}
+
+int main() {
+    test1();
+    test2();
+
+    return 0;
+}
